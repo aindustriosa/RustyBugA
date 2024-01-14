78e20094864f2ae72360e4fa64b4c5db4933aec2
Jorge Muñoz <1619522+punkto@users.noreply.github.com>
2024-01-14 20:10:02 +0100

    5 create a basic board support crate (#6)

---
+++ mightybuga_bsc/examples/no_bsc_blink.rs
@@ -0,0 +1,45 @@
+//! Blinks the Blue Pill's LED
+
+#![no_std]
+#![cfg_attr(not(doc), no_main)]
+use panic_halt as _;
+
+use mightybuga_bsc as board;
+
+use cortex_m_rt::entry;
+use board::hal::{pac, prelude::*};
+
+#[entry]
+fn main() -> ! {
+    // Get access to the core peripherals from the cortex-m crate
+    let cp = cortex_m::Peripherals::take().unwrap();
+    // Get access to the device specific peripherals from the peripheral access crate
+    let dp = pac::Peripherals::take().unwrap();
+
+    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
+    // HAL structs
+    let mut flash = dp.FLASH.constrain();
+    let rcc = dp.RCC.constrain();
+
+    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
+    // `clocks`
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    // Acquire the GPIOC peripheral
+    let mut gpioc = dp.GPIOC.split();
+
+    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
+    // in order to configure the port. For pins 0-7, crl should be passed instead.
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+    //let mut delay = hal::timer::Timer::syst(cp.SYST, &clocks).delay();
+    // or
+    let mut delay = cp.SYST.delay(&clocks);
+
+    loop {
+        delay.delay(300.millis());
+        led.set_high();
+        delay.delay_ms(2_000_u16);
+        led.set_low();
+    }
+}
--- mightybuga_bsc/src/lib.rs
+++ mightybuga_bsc/src/lib.rs
@@ -1,1 +1,73 @@
 #![no_std]
+#![allow(non_camel_case_types)]
+
+
+// reexport hal crates to allow users to directly refer to them
+// like in https://github.com/therealprof/nucleo-f103rb/blob/master/src/lib.rs
+pub use stm32f1xx_hal as hal;
+
+use hal::pac::*;
+use hal::prelude::*;
+use hal::serial::*;
+use hal::timer::SysDelay;
+
+pub use crate::hal::*;
+
+pub mod prelude {
+	pub use cortex_m_rt::entry;
+	pub use stm32f1xx_hal::prelude::{
+		_embedded_hal_blocking_delay_DelayMs, _embedded_hal_blocking_delay_DelayUs, _fugit_ExtU32
+	};
+}
+
+pub struct UART {
+    pub rx: Rx<USART1>,
+    pub tx: Tx<USART1>,
+}
+
+pub struct Leds {
+    pub d1: gpio::Pin<'C', 13, gpio::Output>,
+}
+
+pub struct Mightybuga_BSC {
+    pub delay: SysDelay,
+    pub uart: UART,
+    pub leds: Leds,
+}
+
+impl Mightybuga_BSC {
+    pub fn take() -> Result<Self, ()> {
+        let dp = hal::pac::Peripherals::take().ok_or(())?;
+        let rcc = dp.RCC.constrain();
+        let clocks = rcc.cfgr.freeze(&mut dp.FLASH.constrain().acr);
+
+        let cp = cortex_m::Peripherals::take().unwrap();
+        let mut delay = cp.SYST.delay(&clocks);
+        delay.delay(300.millis());
+
+        let mut afio = dp.AFIO.constrain();
+        let mut gpioa = dp.GPIOA.split();
+        let tx = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
+        let rx = gpioa.pa10;
+        let serial = Serial::new(
+            dp.USART1,
+            (tx, rx),
+            &mut afio.mapr,
+            Config::default()
+                .baudrate(115_200.bps())
+                .wordlength_8bits()
+                .parity_none(),
+            &clocks,
+        );
+        let (tx, rx) = serial.split();
+
+        let mut gpioc = dp.GPIOC.split();
+        let d1: gpio::Pin<'C', 13, gpio::Output> = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+        Ok(Mightybuga_BSC {
+            delay,
+            uart: UART { rx, tx },
+            leds: Leds { d1 },
+        })
+    }
+}
