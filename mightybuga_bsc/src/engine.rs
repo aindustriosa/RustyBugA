1f511e6a1031ced4cf95577bed0c454a62a9b73a
Eloy Coto <eloy.coto@acalustra.com>
2024-01-28 17:39:00 +0100

    feat: Add engine init structure
    This commit adds a way to handle the two motors and a struct who handles
    all of them. The idea is to have an interface which provides to the main
    thread an API to control the movement of the car.
    
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

---
+++ mightybuga_bsc/src/engine.rs
@@ -0,0 +1,46 @@
+use crate::motor::MotorController;
+
+// Engine gets two motors controllers, and has a way to control the car
+pub struct Engine<A: MotorController, B: MotorController> {
+    left: A,
+    right: B,
+}
+
+impl<A: MotorController, B: MotorController> Engine<A, B> {
+    pub fn new(left: A, right: B) -> Self {
+        Engine { left, right }
+    }
+
+    pub fn forward(&mut self, speed: i16) {
+        self.left.start();
+        self.left.set_speed(speed);
+        self.right.start();
+        self.right.set_speed(speed);
+    }
+
+    pub fn backward(&mut self, speed: i16) {
+        self.left.stop();
+        self.left.set_speed(speed);
+        self.right.stop();
+        self.right.set_speed(speed);
+    }
+
+    pub fn left(&mut self, speed: i16, delta: i16) {
+        self.left.start();
+        self.left.set_speed(speed - delta);
+        self.right.start();
+        self.right.set_speed(speed);
+    }
+
+    pub fn right(&mut self, speed: i16, delta: i16) {
+        self.left.start();
+        self.left.set_speed(speed);
+        self.right.start();
+        self.right.set_speed(speed - delta);
+    }
+
+    pub fn stop(&mut self) {
+        self.left.stop();
+        self.right.stop();
+    }
+}
--- mightybuga_bsc/src/lib.rs
+++ mightybuga_bsc/src/lib.rs
@@ -1,7 +1,6 @@
 #![no_std]
 #![allow(non_camel_case_types)]
 
-
 // reexport hal crates to allow users to directly refer to them
 // like in https://github.com/therealprof/nucleo-f103rb/blob/master/src/lib.rs
 pub use stm32f1xx_hal as hal;
@@ -11,20 +10,22 @@ use hal::prelude::*;
 use hal::serial::*;
 use hal::timer::SysDelay;
 
+mod engine;
+mod motor;
+
 pub use crate::hal::*;
 
 pub mod prelude {
-	pub use cortex_m_rt::entry;
-	pub use stm32f1xx_hal::prelude::{
-		_embedded_hal_blocking_delay_DelayMs, _embedded_hal_blocking_delay_DelayUs, _fugit_ExtU32
-	};
+    pub use cortex_m_rt::entry;
+    pub use stm32f1xx_hal::prelude::{
+        _embedded_hal_blocking_delay_DelayMs, _embedded_hal_blocking_delay_DelayUs, _fugit_ExtU32,
+    };
 }
 
 pub struct UART {
     pub rx: Rx<USART1>,
     pub tx: Tx<USART1>,
 }
-
 pub struct Leds {
     pub d1: gpio::Pin<'C', 13, gpio::Output>,
 }
@@ -63,6 +64,21 @@ impl Mightybuga_BSC {
 
         let mut gpioc = dp.GPIOC.split();
         let d1: gpio::Pin<'C', 13, gpio::Output> = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+        // @TODO:
+        // These pins are incorrrect, need to be validated witht the right hardware
+        // I guess that a PWM pin should be here to control the motor's ESC
+        let motor_left = motor::Motor::new(gpioc.pc10.into_push_pull_output(&mut gpioc.crh));
+        let motor_right = motor::Motor::new(gpioc.pc9.into_push_pull_output(&mut gpioc.crh));
+
+        // Engine is the struct which contains all the logics regarding the motors
+        let mut engine = engine::Engine::new(motor_left, motor_right);
+        // This is to validate that everything is in place
+        engine.forward(10);
+        engine.backward(100);
+        engine.left(10, 5);
+        engine.right(10, 5);
+        engine.stop();
 
         Ok(Mightybuga_BSC {
             delay,
