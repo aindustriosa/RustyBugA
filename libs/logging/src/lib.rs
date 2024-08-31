2718cf19b5a9a7484854e4d6d1263a614c2609f5
JatoMixo <jatomixo@proton.me>
2024-08-31 16:51:13 +0200

    fixed typo in comment and added tests

--- libs/logging/src/lib.rs
+++ libs/logging/src/lib.rs
@@ -29,7 +29,7 @@ impl<'a> Logger<'a> {
     pub fn log_u16(&mut self, val: &u16) {
         for digit_index in (1..6).rev() {
 
-            // We have to temporarily convert the u16 to u32, otherwise doing 10⁶5 would cause an
+            // We have to temporarily convert the u16 to u32, otherwise doing 10^5 would cause an
             // overflow in a u16 (100,000 > 65,535)
             let digit = ((*val as u32 % 10u32.pow(digit_index as u32)) / 10u32.pow(digit_index as u32 - 1)) as u16;
             let digit_character = digit + 48; // 48 is the offset of the numbers in the ASCII table
@@ -107,5 +107,21 @@ mod tests {
         let mut logger = Logger::new(&mut mock_writer);
         logger.log("Hello");
         assert_eq!(mock_writer.get_string(), "Hello");
+    }
+
+    #[test]
+    fn test_log_u16() {
+        let mut mock_writer = MockWriter::new();
+        let mut logger = Logger::new(&mut mock_writer);
+        logger.log_u16(&3456u16);
+        assert_eq!(mock_writer.get_string(), "03456");
+    }
+
+    #[test]
+    fn test_log_u16_array() {
+        let mut mock_writer = MockWriter::new();
+        let mut logger = Logger::new(&mut mock_writer);
+        logger.log_u16_array(&[100u16, 250u16, 500u16]);
+        assert_eq!(mock_writer.get_string(), "[00100, 00250, 00500]");
     }
 }
